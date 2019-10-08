e535a1309b5348b966100e68d531d16e46fccf00
Terra Robinson <tlrobinson179@gmail.com>
2019-10-08 00:50:02 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,7 +11,7 @@ var connection = mysql.createConnection({
   password: "1910",
   database: "burgers_db"
 });
-
+};
 
 connection.connect(function(err) {
   if (err) {
